7ca1279730fd4cae6a1ab8b891f209753957d9fd
hecnabae <hecnabae@gmail.com>
2015-04-11 12:32:53 +0200

    MIgration to videogular +v1

--- code/js/main.js
+++ code/js/main.js
@@ -16,38 +16,6 @@ angular.module("videogularApp", [
             }
         };
         $scope.config = {
-            width: 740,
-            height: 500,
-            autoPlay: true,
-            stretch: 1,
-            responsive: true,
-            sources: [
-                {
-                    src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.mp4"),
-                    type: "video/mp4"
-        },
-                {
-                    src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.webm"),
-                    type: "video/webm"
-        },
-                {
-                    src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.ogg"),
-                    type: "video/ogg"
-        }
-    ],
-            tracks: [
-                {
-                    src: "http://www.videogular.com/assets/subs/pale-blue-dot.vtt",
-                    kind: "subtitles",
-                    srclang: "en",
-                    label: "English",
-                    default: ""
-     }
-    ],
-            theme: {
-                url: "http://www.videogular.com/styles/themes/default/latest/videogular.css"
-            },
-
             plugins: {
                 poster: {
                     url: "assets/images/videogular.png"
